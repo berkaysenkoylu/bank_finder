f19de568d8c25c8ba58287197d72c9bcf5b49cd9
berkaysenkoylu <senkoylu_berkay@yahoo.com>
2020-02-10 13:45:47 +0300

    Updates

---
+++ precache-manifest.81d9278a1676e1d470fd942d7dd768b9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fff447f4d27fb24df98f95ac5345f0",
+    "url": "/bank_finder/index.html"
+  },
+  {
+    "revision": "229fe3dd6ec216a70a92",
+    "url": "/bank_finder/static/css/main.2625c5e9.chunk.css"
+  },
+  {
+    "revision": "b2dc3d58ae2a0cbc3e79",
+    "url": "/bank_finder/static/js/2.4e373250.chunk.js"
+  },
+  {
+    "revision": "ac5d9a2ccd3ba848587d2a460762f2f0",
+    "url": "/bank_finder/static/js/2.4e373250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "229fe3dd6ec216a70a92",
+    "url": "/bank_finder/static/js/main.d607d58e.chunk.js"
+  },
+  {
+    "revision": "90ec6abf5c62f506d3f0",
+    "url": "/bank_finder/static/js/runtime-main.6dda7252.js"
+  },
+  {
+    "revision": "08eb00263e4c28ba4f36f3cd25042afd",
+    "url": "/bank_finder/static/media/ankara.08eb0026.jpg"
+  },
+  {
+    "revision": "23bf3d5b2b61031f4e78a8269d8e05b5",
+    "url": "/bank_finder/static/media/bank.23bf3d5b.jpg"
+  },
+  {
+    "revision": "49ff0845ff45f45e1f44c1dbe096329d",
+    "url": "/bank_finder/static/media/compass2.49ff0845.svg"
+  },
+  {
+    "revision": "4b5ad54a6379dbae5aeeeddc278591db",
+    "url": "/bank_finder/static/media/info.4b5ad54a.svg"
+  },
+  {
+    "revision": "25ae61416e937788a8e3ef6f6702a251",
+    "url": "/bank_finder/static/media/library.25ae6141.svg"
+  },
+  {
+    "revision": "1fa7b979aabf5c5dd8ecbde258c3395b",
+    "url": "/bank_finder/static/media/location.1fa7b979.svg"
+  },
+  {
+    "revision": "b2089243af5ff2bb7d5f83109a33e29a",
+    "url": "/bank_finder/static/media/location1.b2089243.svg"
+  },
+  {
+    "revision": "6df50ac4bd074e2289628cd1255e9290",
+    "url": "/bank_finder/static/media/map.6df50ac4.svg"
+  },
+  {
+    "revision": "c083af38e7ed7905c7903a179d6ef547",
+    "url": "/bank_finder/static/media/navigation.c083af38.jpg"
+  },
+  {
+    "revision": "00936e997cc944550b5484c880f7af36",
+    "url": "/bank_finder/static/media/office.00936e99.svg"
+  },
+  {
+    "revision": "9afe8eb13dfcbbe0b239c201d2b21489",
+    "url": "/bank_finder/static/media/search.9afe8eb1.svg"
+  },
+  {
+    "revision": "d5803357a7567386213c6487c287eb57",
+    "url": "/bank_finder/static/media/sprites.d5803357.svg"
+  }
+]);
